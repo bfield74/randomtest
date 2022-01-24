e8844c159080e63ee1835e82b965bf2dd6fced07
Brendan Field <bfield74@gmail.com>
2022-01-24 13:59:58 -0500

    Initial Code

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+from get_random import getr
+import pandas as pd
+
+app = Flask(__name__)
+
+
+@app.route('/random', methods=["POST", "GET"])
+# launch page to the problem.
+def random():
+    rand = getr(0,100)
+    print("here is the random int: ", rand)
+
+    rand10 = getr(0,9)
+    words = pd.read_csv("files/10words.csv")
+    output = words['words'].tolist()
+
+    randword = output[rand10]
+    print("here is the random word we found: ", randword)
+
+    if request.method == "POST":
+        return render_template("random.html", value=rand, word=randword)
+    else:
+        return render_template("random.html", value=-1, word=randword)
+
+if __name__== "__web_output__":
+    app.run(debug=True)
---
+++ get_random.py
@@ -0,0 +1,5 @@
+from random import randint
+
+
+def getr(low, high):
+    return randint(low, high)
